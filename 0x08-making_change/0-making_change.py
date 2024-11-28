982a69984d97c5405edbd8522a02a9dbe7a00c67
Emmanuelbolaji <osundiranemmanuel@gmail.com>
2024-11-28 07:25:52 +0000

    Implement dynamic programming solution for minimum coin change problem

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -19,11 +19,13 @@ def makeChange(coins, total):
     if total <= 0:
         return 0
 
+    coins.sort(reverse=True)
+
     dp = [float('inf')] * (total + 1)
     dp[0] = 0
 
-    for i in range(1, total + 1):
+    for amount in range(1, total + 1):
         for coin in coins:
-            if coin <= i:
-                dp[i] = min(dp[i], dp[i - coin] + 1)
+            if coin <= amount:
+                dp[amount] = min(dp[amount], dp[amount - coin] + 1)
     return dp[total] if dp[total] != float('inf') else -1
