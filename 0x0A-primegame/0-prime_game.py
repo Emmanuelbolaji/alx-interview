106c9c077dec353b93923198f23e3af73b5f35a0
Emmanuelbolaji <osundiranemmanuel@gmail.com>
2024-12-12 11:35:31 -0800

    Implement prime number game with optimal play strategy and winner determination

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -2,6 +2,50 @@
 """Prime Number Game Module."""
 
 
+def is_prime(n):
+    """
+    Check if a number is prime.
+
+    Args:
+        n (int): Number to check
+
+    Returns:
+        bool: True if prime, False otherwise
+    """
+    if n < 2:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+        return True
+
+
+def play_round(n):
+    """
+    Play a single round of the prime game.
+
+    Args:
+        n (int): Upper limit of numbers
+
+    Returns:
+        str: Winner of the round
+    """
+    nums = list(range(1, n + 1))
+    player = 'Maria'
+
+    while True:
+        primes = [x for x in nums if is_prime(x)]
+
+        if not primes:
+            return 'Ben' if player == 'Maria' else 'Maria'
+
+        prime = max(primes)
+
+        nums = [x for x in nums if x % prime != 0]
+
+        player = 'Ben' if player == 'Maria' else 'Maria'
+
+
 def isWinner(x, nums):
     """
     Determine the overall winner across multiple game rounds.
@@ -13,33 +57,18 @@ def isWinner(x, nums):
     Returns:
         str or None: Name of the player with most wins
     """
-    def generate_primes(n):
-        """Generate primes up to n using Sieve of Eratosthenes."""
-        sieve = [True] * (n + 1)
-        sieve[0] = sieve[1] = False
-
-        for i in range(2, int(n**0.5) + 1):
-            if sieve[i]:
-                for j in range(i * i, n + 1, i):
-                    sieve[j] = False
-
-        return [p for p in range(2, n + 1) if sieve[p]]
-
-    def play_round(n):
-        """
-        Play a single round of the prime game.
-
-        Returns:
-            str: Winner of the round
-        """
-        primes = generate_primes(n)
-        return 'Maria' if len(primes) % 2 == 1 else 'Ben'
-
     if not x or not nums:
         return None
 
-    maria_wins = sum(1 for n in nums[:x] if play_round(n) == 'Maria')
-    ben_wins = x - maria_wins
+    maria_wins = 0
+    ben_wins = 0
+
+    for n in nums[:x]:
+        winner = play_round(n)
+        if winner == 'Maria':
+            maria_wins += 1
+        else:
+            ben_wins += 1
 
     if maria_wins > ben_wins:
         return 'Maria'
