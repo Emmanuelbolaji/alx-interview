a38508605a04ca5354644652b067095405b8f469
Emmanuelbolaji <osundiranemmanuel@gmail.com>
2024-12-12 03:58:57 -0800

    Implement prime number game with optimal play strategy and winner determination

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,57 +1,5 @@
 #!/usr/bin/python3
-"""
-Prime Number Game Module
-
-Implements a game where players remove prime numbers and their multiples
-from a set of consecutive integers.
-"""
-
-
-def is_prime(n):
-    """
-    Determine if a number is prime.
-
-    Args:
-        n (int): Number to check for primality
-
-    Returns:
-        bool: True if the number is prime, False otherwise
-    """
-    if n < 2:
-        return False
-
-    for i in range(2, int(n ** 0.5) + 1):
-        if n % i == 0:
-            return False
-
-    return True
-
-
-def play_prime_game(n):
-    """
-    Simulate a single round of the prime number game.
-
-    Args:
-        n (int): Upper limit of consecutive integers
-
-    Returns:
-        str: Winner of the game ('Maria' or 'Ben')
-    """
-    available = set(range(1, n + 1))
-    current_player = 'Maria'
-
-    while True:
-        primes = [num for num in available if is_prime(num)]
-
-    if not primes:
-        return 'Ben' if current_player == 'Maria' else 'Maria'
-
-    prime_choice = max(primes)
-
-    to_remove = {num for num in available if num == prime_choice or num % prime_choice == 0}
-    available -= to_remove
-
-    current_player = 'Ben' if current_player == 'Maria' else 'Maria'
+"""Prime Number Game Module."""
 
 
 def isWinner(x, nums):
@@ -63,26 +11,39 @@ def isWinner(x, nums):
         nums (list): Upper limits for each round
 
     Returns:
-        str or None: Name of the player with most wins, or None if undetermined
+        str or None: Name of the player with most wins
     """
+    def generate_primes(n):
+        """Generate primes up to n using Sieve of Eratosthenes."""
+        sieve = [True] * (n + 1)
+        sieve[0] = sieve[1] = False
+
+        for i in range(2, int(n**0.5) + 1):
+            if sieve[i]:
+                for j in range(i * i, n + 1, i):
+                    sieve[j] = False
+
+        return [p for p in range(2, n + 1) if sieve[p]]
+
+    def play_round(n):
+        """
+        Play a single round of the prime game.
+
+        Returns:
+            str: Winner of the round
+        """
+        primes = generate_primes(n)
+        return 'Maria' if len(primes) % 2 == 1 else 'Ben'
+
     if not x or not nums:
         return None
 
-    maria_wins = 0
-    ben_wins = 0
-
-    for n in nums[:x]:
-        winner = play_prime_game(n)
-        if winner == 'Maria':
-            maria_wins += 1
-        else:
-            ben_wins += 1
+    maria_wins = sum(1 for n in nums[:x] if play_round(n) == 'Maria')
+    ben_wins = x - maria_wins
 
     if maria_wins > ben_wins:
         return 'Maria'
-    elif ben_wins > maria_wins:
+    if ben_wins > maria_wins:
         return 'Ben'
 
     return None
-
-
